c6625778e778ddf332633987c665ee843684325e
Adjei Alexander <alexanderadjei1054@gmail.com>
2024-05-18 15:38:35 +0000

    second commit

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -19,3 +19,33 @@ function updateMyArray (updateArray) {
     return updateArray;
 }
 
+// Task Two: Format Array of Strings
+// This function takes two arrays as input and modifies strings in the second array based on their lengths.
+
+function formatArrayStrings (numArray, stringArray) {
+    // iterate through the array
+    for (let i = 0; i < stringArray.length; i++) {
+         // Check if the length of the string is even or odd and capitalize or convert to lowercase accordingly
+        if (stringArray[i].length % 2 === 0) {
+            stringArray[i] = stringArray[i].charAt(0).toUpperCase() + stringArray[i].slice(1)
+        } else {
+            stringArray[i] = stringArray[i].charAt(0).toLowerCase() + stringArray[i].slice(1)
+        }
+    }
+        // return the updated array in a form of an obj
+    return {
+        numArray: numArray,
+        stringArray: stringArray
+    };
+}
+
+let MyArray = [1, 32, 43, 2, 3, 6, 89];
+let myStringArray = ["dog", "cockroach", "leopard", " monkey", "jagua", "kiwi"]
+
+
+let arrayUpdate = updateMyArray(MyArray);
+let updatedStringArray = formatArrayStrings(arrayUpdate, myStringArray)
+
+console.log(updatedStringArray.numArray);
+console.log(updatedStringArray.stringArray);
+
